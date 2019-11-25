04da535d92ebec0264d66e8a28c117dd3b5f35a6
Senka222 <senka222@gmail.com>
2019-11-25 18:03:03 +0300

    LambdaExpressions commited

---
+++ 7module/1/LambdaExpressions/src/Main.java
@@ -0,0 +1,56 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main
+{
+    private static String staffFile = "data/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args)
+    {
+        ArrayList<Employee> staff = loadStaffFromFile();
+
+        Collections.sort(staff, (o1, o2) -> {
+            int value1 = o1.getSalary().compareTo(o2.getSalary());
+            if (value1 == 0){
+                int value2 = o1.getName().compareTo(o2.getName());
+                return value2;
+            }
+            return value1;
+        });
+
+        for (Employee employee : staff){
+            System.out.println(employee);
+        }
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile()
+    {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try
+        {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for(String line : lines)
+            {
+                String[] fragments = line.split("\t");
+                if(fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employee(
+                    fragments[0],
+                    Integer.parseInt(fragments[1]),
+                    (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
