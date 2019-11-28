0fa58fdfb632c76b57fe84825c4e2403811a2efd
Senka222 <senka222@gmail.com>
2019-11-28 23:59:40 +0300

    airport edited

--- 7module/2/Airport/src/Main.java
+++ 7module/2/Airport/src/Main.java
@@ -1,5 +1,15 @@
+import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
+
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main
 {
@@ -7,6 +17,17 @@ public class Main
     {
         Airport airport = Airport.getInstance();
 
-        airport.getAllAircrafts().stream().filter()
+        airport.getTerminals().stream()
+                .map(terminal -> Arrays.toString(terminal.getFlights().stream()
+                .filter(flight -> flight.getDate().after(Time.valueOf(LocalTime.now())) && flight.getDate().before(Time.valueOf(LocalTime.now().plusHours(2))))
+                .toArray(Flight[]::new))).forEach(System.out::println);
+
+        Aircraft aircraft = new Aircraft("sfsd");
+        Flight flight = new Flight("sdf", Flight.Type.ARRIVAL, Time.valueOf(LocalTime.now().plusHours(1)), aircraft);
+        System.out.println();
+        if (flight.getDate().after(Time.valueOf(LocalTime.now())) &&  flight.getDate().before(Time.valueOf(LocalTime.now().plusHours(2))))
+        {
+            System.out.println("влазит");
+        }
     }
 }
