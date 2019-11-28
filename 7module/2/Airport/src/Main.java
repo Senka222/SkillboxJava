d14880ad5c67ce7efd48276fe1bee7973eb6ce0b
Senka222 <senka222@gmail.com>
2019-11-28 13:04:37 +0300

    Airport commited

---
+++ 7module/2/Airport/src/Main.java
@@ -0,0 +1,12 @@
+import com.skillbox.airport.Airport;
+import com.skillbox.airport.Flight;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Airport airport = Airport.getInstance();
+
+        airport.getAllAircrafts().stream().filter()
+    }
+}
