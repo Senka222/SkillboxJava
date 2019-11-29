b9d2cab7594cee6d2244b55a0e1b9f9983509668
Senka222 <senka222@gmail.com>
2019-11-29 17:19:08 +0300

    Airport editted

--- 7module/2/Airport/src/Main.java
+++ 7module/2/Airport/src/Main.java
@@ -5,10 +5,12 @@ import com.skillbox.airport.Terminal;
 
 import java.sql.Time;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 import java.util.function.Predicate;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main
@@ -19,15 +21,21 @@ public class Main
 
         airport.getTerminals().stream()
                 .map(terminal -> Arrays.toString(terminal.getFlights().stream()
-                .filter(flight -> flight.getDate().after(Time.valueOf(LocalTime.now())) && flight.getDate().before(Time.valueOf(LocalTime.now().plusHours(2))))
-                .toArray(Flight[]::new))).forEach(System.out::println);
+                        .filter(flight -> flight.getDate().after(Time.valueOf(LocalTime.now())) && flight.getDate().before(Time.valueOf(LocalTime.now().plusHours(2))))
+                        .toArray(Flight[]::new))).forEach(System.out::println);
 
-        Aircraft aircraft = new Aircraft("sfsd");
+ /*       Aircraft aircraft = new Aircraft("sfsd");
         Flight flight = new Flight("sdf", Flight.Type.ARRIVAL, Time.valueOf(LocalTime.now().plusHours(1)), aircraft);
         System.out.println();
         if (flight.getDate().after(Time.valueOf(LocalTime.now())) &&  flight.getDate().before(Time.valueOf(LocalTime.now().plusHours(2))))
         {
             System.out.println("влазит");
         }
+        else System.out.println("не влазит");
+
+        System.out.println(Time.valueOf(LocalTime.now()));
+        System.out.println(flight.getDate());
+        System.out.println(Time.valueOf(LocalTime.now().plusHours(2)));
+*/
     }
 }
